b326884d6015b3dadb2149b9a64e270ac49fbb2c
ajitk7 <ajit.kulkarni167@gmail.com>
2020-12-23 18:34:52 +0530

    bizzare String assignment

--- bizarreStringIncrementer.js
+++ bizarreStringIncrementer.js
@@ -1,1 +1,16 @@
 // Start your implementation here
+function bizarreStringIncrementer(bizarreString){
+
+let patternMatcher = "/\d+/";
+matches = bizarreString.match(/\d+/g); 
+let numberStr =matches[matches.length-1];
+let number =parseInt(numberStr);
+if((number+1).toString().length=== numberStr.length){
+    return bizarreString.replace(numberStr,parseInt(number)+1);
+}else{ 
+    return bizarreString.replace(numberStr,""+numberStr.charAt(0)+""+ (number+1));
+}
+
+}
+
+module.exports = { bizarreStringIncrementer };
